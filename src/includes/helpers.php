a8a0af36cfa675abeafade51b0655fec9c5ee11c
Vid Luther <vid@luther.io>
2021-07-17 16:00:55 -0500

    Adding Account name, instead of account id to the CSV

--- src/includes/helpers.php
+++ src/includes/helpers.php
@@ -1,94 +1,108 @@
-<?php 
-namespace Wpe; 
+<?php
+namespace Wpe;
 
 use GuzzleHttp\Client;
-use GuzzleHttp\Exception\RequestException;
-use GuzzleHttp\Pool;
 use GuzzleHttp\Psr7\Request;
-use GuzzleHttp\Psr7\Response;
 
+class Api
+{
+    private $_client;
+    public $status;
 
-class Api {
-    private $client; 
-    public $status; 
-
-    public function __construct() {
-        $this->client = new Client([
+    public function __construct()
+    {
+        $this->_client = new Client([
             // Base URI is used with relative requests
             'base_uri' => BASE_URL,
             // You can set any number of default request options.
-            'timeout'  => 2.0,
+            'timeout' => 2.0,
         ]);
 
     }
 
-    public function checkStatus() {
-       $response =  $this->client->request('GET', 'status');
+    public function checkStatus()
+    {
+        $response = $this->_client->request('GET', 'status');
 
-       if($this->checkIfResponseIsValid($response)) {
-            $this->status = true; 
-       } else {
-           $this->status = false; 
-       }
+        if ($this->checkIfResponseIsValid($response)) {
+            $this->status = true;
+        } else {
+            $this->status = false;
+        }
 
-       return $this->status; 
+        return $this->status;
     }
 
     public function getSites()
     {
-        // Get a list of sites at WPE, this will give us a list of installs automatically. 
+        // Get a list of sites at WPE, this will give us a list of installs automatically.
 
-        $response = $this->request('GET', 'sites'); 
-        if($this->checkIfResponseIsValid($response)) {
-            $body = $response->getBody(); 
-            $sitesCollection = json_decode($body); 
-            $siteResults = $sitesCollection->results; 
-            return $siteResults; 
+        $response = $this->request('GET', 'sites');
+        if ($this->checkIfResponseIsValid($response)) {
+            $body = $response->getBody();
+            $sitesCollection = json_decode($body);
+            $siteResults = $sitesCollection->results;
+            return $siteResults;
         } else {
-            return false; 
+            return false;
         }
 
     }
 
-    public function getInstallDomain($install_id) {
-        $install_details = $this->getInstallDetails($install_id); 
-        return $install_details->primary_domain; 
-    }
+    public function getAccountName($account_id)
+    {
+        $response = $this->request('GET', "accounts/$account_id");
 
-    public function getInstallDetails($install_id) {
-        $response = $this->request('GET', "installs/$install_id"); 
-       
-        if($this->checkIfResponseIsValid($response)) {
-            $body = $response->getBody(); 
-            $install_details = json_decode($body); 
-            return $install_details;   
+        if ($this->checkIfResponseIsValid($response)) {
+            $body = $response->getBody();
+            $account_details = json_decode($body);
+            return $account_details->name;
         } else {
-            return false; 
+            return false;
         }
-    }
-
-    private function request($method, $endpoint) {
-        if(DEBUG === true) { 
-            echo "Making a $method request to $endpoint" . PHP_EOL; 
-        }
-        $response = $this->client->request($method, $endpoint, ['auth' => [WPE_USERNAME, WPE_PASSWORD]]); 
-        return $response; 
 
     }
 
-    private function checkIfResponseIsValid($response) {
-        $rcode = $response->getStatusCode(); 
+    public function getInstallDomain($install_id)
+    {
+        $install_details = $this->getInstallDetails($install_id);
+        return $install_details->primary_domain;
+    }
 
-        if($rcode === 200) {
-            #echo "WP Engine API is Up and Running" . PHP_EOL; 
-            return true; 
+    public function getInstallDetails($install_id)
+    {
+        $response = $this->request('GET', "installs/$install_id");
+
+        if ($this->checkIfResponseIsValid($response)) {
+            $body = $response->getBody();
+            $install_details = json_decode($body);
+            return $install_details;
         } else {
-            echo 'The API Responded with error ('. $rcode . ')' . PHP_EOL; 
-            return false; 
+            return false;
         }
     }
 
-   
-    
-   
+    private function request($method, $endpoint)
+    {
+        if (DEBUG === true) {
+            echo "Making a $method request to $endpoint" . PHP_EOL;
+        }
+        $response = $this->_client->request($method, $endpoint, ['auth' => [WPE_USERNAME, WPE_PASSWORD]]);
+        return $response;
+
+    }
+
+    private function checkIfResponseIsValid($response)
+    {
+        $rcode = $response->getStatusCode();
+
+        if ($rcode === 200) {
+            #echo "WP Engine API is Up and Running" . PHP_EOL;
+            return true;
+        } else {
+            echo 'The API Responded with error (' . $rcode . ')' . PHP_EOL;
+            return false;
+        }
+    }
+
 }
--- src/index.php
+++ src/index.php
@@ -1,57 +1,53 @@
 <?php
 
 require 'vendor/autoload.php';
-require_once __DIR__ . '/config.php'; 
+require_once __DIR__ . '/config.php';
 
-
-
-use Wpe\Api as wpe; 
 use League\Csv\Writer;
+use Wpe\Api as wpe;
 
-$client = new Wpe(); 
+$client = new Wpe();
 
+if ($client->checkStatus() === false) {
 
-if($client->checkStatus() === false) {
-    
-    echo "WPE Api System seems to be down..or we wrote crappy code" . PHP_EOL; 
-    exit(1); 
+    echo "WPE Api System seems to be down..or we wrote crappy code" . PHP_EOL;
+    exit(1);
 
 }
 
-$sites = $client->getSites(); 
+$sites = $client->getSites();
 
-
-$csvHeader = ['account_id', 'site_id', 'site name', 'install id', 'install name', 'install environment', 'install cname', 'php version', 'is multisite', 'primary domain']; 
+$csvHeader = ['account_name', 'site_id', 'site name', 'install id', 'install name', 'install environment', 'install cname', 'php version', 'is multisite', 'primary domain'];
 //we create the CSV into memory
 $csv = Writer::createFromFileObject(new SplTempFileObject());
 
 //insert the header
 $csv->insertOne($csvHeader);
 
-$csvRecords = array(); 
-    foreach($sites as $site) {
-        $account_id  = $site->account->id; 
-        $site_id = $site->id; 
-        $site_name = $site->name; 
-        $installs = $site->installs; 
-        
+$csvRecords = array();
+foreach ($sites as $site) {
+    // This is another call to the API.. we should probably make a local map
+    // of account ids to account names
+    $account_name = $client->getAccountName($site->account->id);
+    $site_id = $site->id;
+    $site_name = $site->name;
+    $installs = $site->installs;
 
-        
-        // iterate through installs
-        foreach($installs as $install) {
-            $install_id = $install->id; 
-            $install_name = $install->name; 
-            $install_env = $install->environment; 
-            $install_cname = $install->cname; 
-            $install_phpversion = $install->php_version; 
-            $install_is_multisite = $install->is_multisite; 
-            // If we want to know the domain associated with the site, we need to call /installs/$install_id at WPE 
-            $install_primary_domain = $client->getInstallDomain($install_id); 
-            //$install_primary_domain = "doo.com"; // this was added here just to speed up execution during testing. 
-            $csvRecords[] = [$account_id,$site_id, $site_name, $install_id, $install_name, $install_env, $install_cname, $install_phpversion, $install_is_multisite, $install_primary_domain]; 
+    // iterate through installs
+    foreach ($installs as $install) {
+        $install_id = $install->id;
+        $install_name = $install->name;
+        $install_env = $install->environment;
+        $install_cname = $install->cname;
+        $install_phpversion = $install->php_version;
+        $install_is_multisite = $install->is_multisite;
+        // If we want to know the domain associated with the site, we need to call /installs/$install_id at WPE
+        $install_primary_domain = $client->getInstallDomain($install_id);
+        //$install_primary_domain = "doo.com"; // this was added here just to speed up execution during testing.
+        $csvRecords[] = [$account_name, $site_id, $site_name, $install_id, $install_name, $install_env, $install_cname, $install_phpversion, $install_is_multisite, $install_primary_domain];
 
-        } 
     }
-    $csv->insertAll($csvRecords); 
-    $csv->output('wpesites.csv'); 
- die; 
+}
+$csv->insertAll($csvRecords);
+$csv->output('wpesites.csv');
+die;
